922dcefe9f2284e6bd05ce41f57e3fe0de5d38c9
Saunack <kr.saunack@gmail.com>
2024-06-10 23:38:51 -0700

    bugfix

--- tests/regression/amo/kernel.cpp
+++ tests/regression/amo/kernel.cpp
@@ -9,7 +9,7 @@ void kernel_body(int task_id, kernel_arg_t* __UNIFORM__ arg) {
 	auto src1_ptr = reinterpret_cast<TYPE*>(arg->src1_addr);
 	auto dst_ptr  = reinterpret_cast<TYPE*>(arg->dst_addr);
 
-	dst_ptr[task_id] = vx_amo_add(src0_ptr[task_id],src1_ptr[task_id])+src1_ptr[task_id];
+	dst_ptr[task_id] = vx_amo_add(src0_ptr[task_id],src1_ptr[task_id]);
 }
 
 int main() {
