b7a5c4fa0fb4e1d5ae356444295cea7bee04ebf0
Saunack <kr.saunack@gmail.com>
2024-06-12 13:25:12 -0700

    change test case to increment

--- tests/regression/amo/kernel.cpp
+++ tests/regression/amo/kernel.cpp
@@ -9,9 +9,10 @@ void kernel_body(int task_id, kernel_arg_t* __UNIFORM__ arg) {
 	auto src1_ptr = reinterpret_cast<TYPE*>(arg->src1_addr);
 	auto dst_ptr  = reinterpret_cast<TYPE*>(arg->dst_addr);
 
-	// dst_ptr[task_id] = vx_amo_add(src0_ptr+task_id,src1_ptr+task_id)+src1_ptr[task_id];
+	auto x = src0_ptr[task_id]+10000;
 	dst_ptr[task_id] = vx_amo_add(src0_ptr[task_id],src1_ptr[task_id]);
-	vx_printf("LSU_LW & ADD instruction: %d %d %d %d\n",*(src0_ptr+task_id),*(src1_ptr+task_id),src0_ptr[task_id]+src1_ptr[task_id],dst_ptr[task_id]);
+	// should be sum, 2nd operand, write value, 2nd operand - 10000
+	vx_printf("LL SC Amoadd clobber: %d %d %d %d\n",src0_ptr[task_id],src1_ptr[task_id],dst_ptr[task_id],x-src0_ptr[task_id]);
 }
 
 int main() {
